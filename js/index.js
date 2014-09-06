1598ba76dd12de798b2994be4a02b74a8f5ce7e6
Kathy Wu <kwu01@risd.edu>
2014-09-06 16:28:45 -0400

    First working site!
    Thanks Willy. (Christmas present mode, + work from Digpo.)

---
+++ js/index.js
@@ -0,0 +1,141 @@
+
+for (var i = 0; i < work.length; i++) {
+	var piece=work[i];
+	$("#work").append(
+		$("<a href=\"#"+piece.folder+"\"></a>").append(
+			"<div class=\"image-tile\" style=\"background-image:url(work/"+piece.folder+"/"+piece.thumb+");\"></div>"
+		).append(
+			$("<figcaption></figcaption>").html(piece.name)
+		)
+	)
+}
+//Responds to a hash change. Scrolls to work, connect, or about, or puts the content of a given folder (given by hash) into the lightbox, and optionally fades if not in a hurry.
+var activate = function(hurry,overrideHash){
+	var hash=(overrideHash||window.location.hash).replace(/#/g,""), piece;
+	$.scrollTo.window().stop(true);
+	if(hash=="about"||hash=="connect"||hash=="work"||hash==""){
+		if(!hurry){
+			$.scrollTo("#"+(hash||"splash"),1000,function(){
+				window.location.hash=hash;
+			});
+		}
+	}
+	else if(piece=getByFolder(hash)){
+		var imageRegex=/\[([^\[\]]+)\]/g, underlineRegex=/\*([^\*]+)\*/g, centerRegex = /\{([^\{\}]+)\}/g;
+		var callback=function(){
+			var content=piece.content.replace(/\[/g,"\n[").replace(/\]/,"]\n").replace(/\n+/g,"\n").replace(/^\s+|\s+$/g,"").split("\n"),contentString="";
+			for (var i = 0; i < content.length; i++) {
+				if(content[i].search(imageRegex)>=0){
+					contentString+=content[i].replace(imageRegex,function(a,b){return "<img src=\"work/"+piece.folder+"/"+b+"\">"});
+				}
+				else{
+					contentString+="<div"+(content[i].search(centerRegex)>=0?" class=\"center-align\"":"")+">"+content[i].replace(centerRegex,function(a,b){return b;}).replace(underlineRegex,function(a,b){return "<span class=\"highlight\">"+b+"</span>"})+"</div>"
+				}
+
+			};
+			$("#content").html("<h3>"+piece.name+"</h3><br>"+contentString);
+			$("#lightbox").show();
+			$.scrollTo("#lightbox",0);
+		}
+		if(hurry){
+			callback();
+		}
+		else{
+			$("body").stop(true).fadeTo(200,0,function(){
+				callback();
+				$("body").fadeTo(500,1);
+			});
+		}
+	}
+
+}
+var deactivate = function(){
+	$("#lightbox").slideUp(500);
+}
+var getByFolder = function(folderName){
+	for (var i = 0; i < work.length; i++) {
+		if(work[i].folder==folderName){
+			return work[i];
+		}
+	}
+	return null;
+}
+var currentQuote=0;
+var nextQuote = function(){
+	var quote=quotes[currentQuote];
+	$("#quote").html(quote.quote);
+	$("#quote-source").html("&ndash; "+quote.from+"<br>"+quote.title+(quote.company?", "+quote.company:""))
+	currentQuote++;
+	if(currentQuote>=quotes.length){
+		currentQuote=0;
+	}
+	$("#quote-container").fadeTo(1000,1).delay(5000).fadeTo(1000,0,function(){nextQuote()})
+}
+
+// var currentSplash=0;
+// var nextSplash = function(){
+// 	$("#splash").attr("style","background-image:url('splash/"+splashes[currentSplash]+"')");
+// 	currentSplash++;
+
+// 	if(currentSplash>=splashes.length){
+// 		currentSplash=0;
+// 	}
+
+// 	setTimeout(nextSplash,5000);
+
+// }
+
+$(document).ready(function(){
+	if(getByFolder(window.location.hash.replace(/#/g,""))){
+		activate(true);
+	}
+	$("#logo,nav .links").click(function(event){
+		var hash=$(event.target).is("a")?$(event.target).attr("href"):$(event.target).parent().attr("href");
+		activate(false,hash);
+		event.preventDefault();
+	})
+	quotes.sort(function(a,b){return Math.random()-0.5})
+	nextQuote();
+	// nextSplash();
+})
+$(window).on("hashchange",function(){
+	activate();
+})
+var tempAmount=0,prevAmount;
+$(window).scroll(function(){
+	tempAmount=$(window).scrollTop();
+})
+
+var raf=window.webkitRequestAnimationFrame||window.requestAnimationFrame;
+var update = function(){
+	var amount = tempAmount/$(window).innerHeight();
+	if(prevAmount!=tempAmount){
+		$("#name").css("opacity", 0.95*clamp(1-2*amount, 0, 1));
+		$("#splash").css("background-position", "center "+clamp(50-amount*40, 0, 100)+"%");		
+	}
+	prevAmount=tempAmount;
+	if(raf){
+		raf(arguments.callee);
+	}
+	else{
+		setTimeout(arguments.callee,1000/60);
+	}
+}
+update();
+
+function clamp(number, min, max){
+	if(number < min){
+		return min;
+	}
+
+	if(number > max){
+		return max;
+	}
+
+	else{
+		return number;
+	}
+
+};
+
+
---
+++ work/work.js
@@ -0,0 +1,119 @@
+work=[
+ 	
+ 	{
+		"folder":"wildest-flowers",
+		"name":"Wildest Flowers",
+		"thumb":"wildest-flowers.png",
+		"content":"An exploration of duality and the color wheel for Aki Nurosi&rsquo;s Color Theory class. We were prompted to look at how the same color reacts differently on a black vs. a white background. I created these seed packets for flowering weeds like dandelions and clovers. RISD 2013.\n[wildest-flowers-1.jpg]\n[wildest-flowers-2.jpg]\n[wildest-flowers-3.jpg]\n[wildest-flowers-4.jpg]\n[wildest-flowers-5.jpg]\n[wildest-flowers-6.jpg]\n[wildest-flowers-7.jpg]\n[wildest-flowers-8.jpg]\n[wildest-flowers-9.jpg]\n[wildest-flowers-10.jpg]"
+	},
+
+	{	"folder":"hyphen",
+		"name":"The Hyphen Space",
+		"thumb":"screen2.png",
+		"content":"A personal project. I shared my experience with bilinguality and growing up Chinese-American, focusing on this metaphorical 'hyphen space' between the two. I chose web as a medium because it lent itself well to instant translated and simultaneous language. I crafted illustrations and essays comparing Eastern and Western philosophies on Success, Friendship, Happiness, and Health. On hover, the English text in each sentence transforms into Chinese Characters.\n[screen2.png]\n[screen1.png]\n[screen3.png]\n[screen4.png]\n[spacer.png]\n{Demo}\n[demo2.gif]\n[spacer.png]\n"
+	},
+
+
+	{
+		"folder":"chocolee",
+		"name":"Chocolee",
+		"thumb":"chocolee.png",
+		"content":"Inspired by origami and Japanese wrapping cloths called furoshiki, &ldquo;Chocolee,&rdquo; chocolates feature patterns designed with both collaged and digitally drawn food elements. Each square wrapper can be easily repurposed for paper-folding and crafting.\nThis imaginary chocolate brand emphasizes not only beautiful wrapping but the fun in its re-use, with paper you might give a second life as gift wrap or even a paper crane. By paying attention to the art of wrapping, we bring moments of aesthetic pleasure and play into everyday life.\nFeatured on Packaging Design Served, Behance Student Show, and The Design Blog, Dec 2013.\n[chocolee.png]\n[chocolee-flavors.gif]\n[chocolee-set-2.jpg]\n[chocolee-business.jpg]\n[chocolee-business-set.jpg]\n[chocolee-pepper-3.jpg]"
+	},
+	
+	{
+    	"folder":"mia",
+		"name":"Miami International Airport",
+		"thumb":"mia8.png",
+		"content":"Redesigned directory for the Miami Internation Airport. It was really important that the interface was legible, streamlined, and friendly. My target user was a tired traveler, perhaps experiencing a layover with time to burn. I wanted searching to be efficient and exploration to be fun.\nCheck out the <u><a href='https://projects.invisionapp.com/share/Q2UMXDGF' target='_blank'>InVision prototype.</u></a>\n[mia8.png]\n[mia2.png]\n[mia3.png]\n[mia4.png]\n[mia5.png]\n[mia6.png]\n[mia7.png]"
+	},
+
+	
+
+	{
+		"folder":"places",
+		"name":"Oh, The Places You&rsquo;ll See!",
+		"thumb":"places.png",
+		"content":"A study of simultaneous contrast, the phenomenon where surrounding colors cause two different colors to look relatively more or less similar.\nMy family loves to travel, and this postcard set compiles many of the places I&rsquo;ve seen while looking at color relationships.\nEach postcard has a fun little blurb for what to do during day or night.\n[places-1.jpg]\n[places-2.gif]\n[places-3.jpg]\n[places-4.jpg]\n[places-5.jpg]\n[places-6.gif]\n[places-7.jpg]\n[places-8.jpg]\n[places-9.jpg]\n[places-10.gif]"
+	},
+	// {
+	// 	"folder":"berlewi",
+	// 	"name":"Berlewi",
+	// 	"thumb":"berlewi.png",
+	// 	"content":"An accordion-fold timeline on the carefully researched life & work of Polish Constructivist, Henryk Berlewi. The challenge here was channeling Berlewi&rsquo;s work from decades ago while designing in the spirit of 2015. \nI used Consolas for the body type and RISD&rsquo;s own ZXX typeface for the headers, designed by Sang Mun &rsquo;12.\n[berlewi-1.png]\n[berlewi-2.png]\n\n[berlewi-3.png]\n[berlewi-4.png]\n[berlewi-5.png]\n[berlewi-6.png]\n[berlewi-7.png]\n[berlewi-8.png]\n[berlewi-9.png]\n[berlewi-10.png]\n[berlewi-11.png]\n[berlewi-12.png]\n[berlewi-13.png]\n[berlewi-14.png]"},
+	
+	{
+		"folder":"frutiger",
+		"name":"Frutiger",
+		"thumb":"frutiger.png",
+		"content":"{Set of panels on the life and work of Swiss type designer Adrian Frutiger. 2013.}\n[Panel-1.jpg]\n[Panel-2.jpg]\n[Panel-3.jpg]"
+	},
+	{
+		"folder":"curious",
+		"name":"The Curious Incident",
+		"thumb":"curious.png",
+		"content":"I typeset Mark Haddon&rsquo;s Curious Incident of the Dog in the Night-time, a story told from the point of view of an autistic savant named Christopher. This project was a great opportunity to explore Christopher&rsquo;s systematic world through systems of typographic formatting and color. \nHere, I strove to create a dialogue between the published material and the author by exposing Christopher&rsquo;s strange, quirky voice. Using colored overprinting, I literally introduced a second layer of reading.\n[curious-1.jpg]\n[curious-2.jpg]\n[curious-3.jpg]\n[curious-4.jpg]\n[curious-5.jpg]\n[curious-6.jpg]\n[curious-7.jpg]\n[curious-8.jpg]\n[curious-9.jpg]\n[curious-10.jpg]\n[curious-11.jpg]"
+	},
+	// {
+	// 	"folder":"lp",
+	// 	"name":"Landmark Poster",
+	// 	"thumb":"lp.png",
+	// 	"content":"Hey there, funny-looking. Recognize this shape? That&rsquo;s the Volkswagen 1959 Sedan. Once compared to a Coca-Cola bottle in iconic status.\nVolkswagen poster detailing the influence of the Doyle Dane and Bernbach&rsquo;s magnificent Volkswagen ads of the 1950s. The VW think small campaign left a huge legacy.\n[thinking-BIG.jpg]"
+	// },
+
+
+	// {
+	// 	"folder":"19",
+	// 	"name":"19: Artist&rsquo;s Book",
+	// 	"thumb":"19.png",
+	// 	"content":"An artist&rsquo;s book about how growing up works. Inspired by the text, &ldquo;Eleven&rdquo; by Sandra Cisneros.\nWhat they don&rsquo;t understand about birthdays and what they never tell you is that when you&rsquo;re eleven, you&rsquo;re also ten, and nine, and eight, and seven, and six, and five, and four, and three, and two, and one...\nBecause the way you grow old is kind of like an onion or like the rings inside a tree trunk or like my little wooden dolls that fit one inside the other, each year inside the next one.\n[19-1.jpg]\n[19-2.jpg]\n[19-3.jpg]\n[19-4.jpg]\n[19-5.jpg]\n[19-6.jpg]\n[19-7.jpg]\n[19-8.jpg]\n[19-9.jpg]\n[19-sketch.jpg]\n"
+	// },
+	
+	{	"folder":"google-backpack",
+		"name":"Google Backpack",
+		"thumb":"GPthumb.png",
+		"content":"Created during a UI-UX RISD Studio as a collaboration with Brown University student Yutong Liao. We proposed Google Backpack as an online space optimized for education that connects Google Drive, Google Calendar, and Gmail, with the added functionality of grading, assignment management, as well as online-offline and cross-app synchronization. The idea came in response to our own frustrations as college students using E-Portfolio websites to learn and file-share. We observed that many of our peers also craved a more streamlined education experience online. \n We chose Google because many universities already rely on Gmail and Drive services. By utilizing apps the majority of internet users are already comfortable with, we save users the effort of committing to an unfamiliar system. \n[GP.png]\n[GP2.png]\n[GP3.png]\n[GP4.png]\n[GP5.png]\n{Professor Side}\n[GP6.png]\n[GP7.png]\n[GP8.png]\n[spacer.png]\n{Student Side}\n[GP9.png]\n[GP11.png]\n[GP10.png]\n[GP12.png]\n"
+	},
+
+
+
+	/*{
+    	"folder":"pnp",
+		"name":"Paper Nor Plastic",
+		"thumb":"pnp.png",
+		"content":"{A poster triptych on the everyday impact our choices can make.}\n[plastic.jpg]\n[paper.jpg]\n[byo.jpg]\n"
+	},*/
+	/*{
+		"folder":"kathy-friends",
+		"name":"Kathy + Friends",
+		"thumb":"kathy-friends.png",
+		"content":"{A textile featuring some of the most important people in my life in 2012.}\n[repeat.jpg]\n[close-up.jpg]\n[original.jpg]\n"
+	}*/
+
+	
+]
+
+quotes=[
+	{"quote":"Kathy Wu is a critical thinker, keen strategist, and prolific designer.","from":"Christine Needham","title":"Co-founder","company":"Proverb"},
+	{"quote":"She is a pleasure to work with. I am looking forward to seeing her growth!","from":"Christine Needham","title":"Co-founder","company":"Proverb"},
+	{"quote":"She is a very talented artist who listens well to understand the style ethos.","from":"Sunanda Sahay","title":"Director","company":"Essence of India"},
+	{"quote":"She has the sensitivity to appreciate the cultural sensitivities of a different community.","from":"Sunanda Sahay","title":"Director","company":"Essence of India"},
+	{"quote":"She worked quickly through multiple design iterations and achieved results that were superior to our original ideas.","from":"Sunanda Sahay","title":"Director","company":"Essence of India"},
+	{"quote":"She is a pleasure to work with and I look forward to working with her again and again.","from":"Sunanda Sahay","title":"Director","company":"Essence of India"},
+	{"quote":"Kathy is detailed-oriented, organized, and very thoughtful about her work. She's creative with great ideas&mdash; a pleasure to work with!","from":"Luyi Shao","title":"Founder","company":"RI Kung Fu Club"},
+	{"quote":"Fully motivated and hard working. Final products were creatively thoughtful and beautifully executed.","from":"Akefeh Nurosi","title":"Faculty","company":"RISD"},
+	{"quote":"Kathy is a serious, mature, and competent designer with a pleasant personality.","from":"Akefeh Nurosi","title":"Faculty","company":"RISD"}
+]
+
+
+
+/*Kathy's hew hew area*/
+/*\n[chocolee-milk.jpg]\n[chocolee-white.jpg]\n[chocolee-dark.jpg]\n[chocolee-orange.jpg]\n[chocolee-raspberry.jpg]\n[chocolee-hazelnut.jpg]\n[chocolee-mint.jpg]\n[chocolee-pepper-2.jpg]*/
+
+splashes=[
+	"splash-1.jpg",
+	"splash-5.jpg",
+	"splash-2.jpg",
+	"splash-3.jpg",
+	"splash-4.jpg"
+]
