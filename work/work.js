be730cd6e7f00d20d6816334e9a86bef808901ef
Kathy Wu <kwu01@risd.edu>
2015-02-10 21:10:39 -0500

    New projects, still some desc. to be fixed.

--- work/work.js
+++ work/work.js
@@ -1,18 +1,53 @@
 work=[
  	
- 	{
-		"folder":"wildest-flowers",
-		"name":"Wildest Flowers",
-		"thumb":"wildest-flowers.png",
-		"content":"An exploration of duality and the color wheel for Aki Nurosi&rsquo;s Color Theory class. We were prompted to look at how the same color reacts differently on a black vs. a white background. I created these seed packets for flowering weeds like dandelions and clovers. RISD 2013.\n[wildest-flowers-1.jpg]\n[wildest-flowers-2.jpg]\n[wildest-flowers-3.jpg]\n[wildest-flowers-4.jpg]\n[wildest-flowers-5.jpg]\n[wildest-flowers-6.jpg]\n[wildest-flowers-7.jpg]\n[wildest-flowers-8.jpg]\n[wildest-flowers-9.jpg]\n[wildest-flowers-10.jpg]"
-	},
 
-	{	"folder":"hyphen",
+    
+{
+		"folder":"turrell",
+		"name":"James Turrell Museum",
+		"thumb":"thumb2.png",
+		"content":"Explanation text.\n[turrell9.png]\n[turrell10.png]\n[spacer.png]\n <p style='font-weight:bold;text-transform:uppercase;letter-spacing:.5px;'>{Applications}</style>\n[stationary.jpg]\n[webstuff.jpg]\n[brochures.jpg]\n[poster.jpg]\n[turrell12.png]\n[spacer.png]\n<p style='font-weight:bold;text-transform:uppercase;letter-spacing:.5px;'>{Temporal tickets}</style>\nThe way a Turrell piece transforms when viewed during different times of the day inspired these dynamic tickets. The museum, open from 10:00AM to 6:00PM daily, issues a different colored ticket to visitors each hour. Viewed as a long strip, the tickets appear as a continuous gradient. The colors, taken from the museum palette, are meant to echo the temporal radiance of the sky.\n[tickets.png]\n[spacer.png]\n<p style='font-weight:bold;text-transform:uppercase;letter-spacing:.5px;'>{Abstract Garden}</style>\n My engagement piece brings visitors together in an outdoor garden which supplements Turrell’s work. I imagined a tranquil space that visitors would use to reflect and relax. The pool connects to an unseen irrigation system which waters the grape vines crawling along the trellises. In this way, the piece feeds back into the terrain, paying homage to the relationship between abstracted nature and the colorful beauty of the arid landscape.\n[engagement.png]\n"
+	},
+        
+{
+		"folder":"keep",
+		"name":"Keep",
+		"thumb":"thumb2.png",
+		"content":"An exploration of duality and the color wheel for Aki Nurosi&rsquo;s Color Theory class. We were prompted to look at how the same color reacts differently on a black vs. a white background. I created these seed packets for flowering weeds like dandelions and clovers. RISD 2013.\n[keep-splash.png]\n[keep-intro.png]\n[keep-intro2.png]\n[spacer.png]\n<p style='font-weight:bold;text-transform:uppercase;letter-spacing:.5px;'>{USER STORY}</style>\n An exploration of duality and the color wheel for Aki Nurosi&rsquo;s Color Theory class. We were prompted to look at how the same color reacts differently on a black vs. a white background. I created these seed packets for flowering weeds like dandelions and clovers. RISD 2013.\n<div class ='divider'></div>\n[keep.png]\n[keep2.png]\n[keep3.png]\n[keep4.png]\n[keep6.png]\n[keep7.png]\n[keep8.png]\n[keep9.png]"
+	},
+        
+    
+    {	"folder":"hyphen",
 		"name":"The Hyphen Space",
-		"thumb":"screen2.png",
+		"thumb":"thumb.png",
 		"content":"A personal project. I shared my experience with bilinguality and growing up Chinese-American, focusing on this metaphorical 'hyphen space' between the two. I chose web as a medium because it lent itself well to instant translated and simultaneous language. I crafted illustrations and essays comparing Eastern and Western philosophies on Success, Friendship, Happiness, and Health. On hover, the English text in each sentence transforms into Chinese Characters.\n[screen2.png]\n[screen1.png]\n[screen3.png]\n[screen4.png]\n[spacer.png]\n{Demo}\n[demo2.gif]\n[spacer.png]\n"
 	},
+    
 
+
+
+//	{
+//    	"folder":"mia",
+//		"name":"Miami International Airport",
+//		"thumb":"mia8.png",
+//		"content":"Redesigned directory for the Miami Internation Airport. It was really important that the interface was legible, streamlined, and friendly. My target user was a tired traveler, perhaps experiencing a layover with time to burn. I wanted searching to be efficient and exploration to be fun.\nCheck out the <u><a href='https://projects.invisionapp.com/share/Q2UMXDGF' target='_blank'>InVision prototype.</u></a>\n[mia8.png]\n[mia2.png]\n[mia3.png]\n[mia4.png]\n[mia5.png]\n[mia6.png]\n[mia7.png]"
+//	},
+
+	 {
+    	"folder":"touchart",
+		"name":"TouchArt",
+		"thumb":"ta.png",
+		"content":"How has digital proliferation changed they way we access famous images? Works of art that once existed as location-specific, single editions are now available to save and manipulate on personal computers.\nIn this virtual gallery space, Touch Art,I invited peers and fellow art-lovers to bring themselves closer to their favorite paintings by digitally “touching,” works of art. Through a specific set of instructions, I managed to collect a slew of famous masterworks that had been intruded on by another’s hand. The original set of instructions was publicized on Facebook, but other versions were later distributed through email, Tumblr, and word of mouth.\nContribute your favorite painting at <a href='http://www.touch-art.tumblr.com' target='blank' style='font-weight:bold'>touch-art.tumblr.com</a>\n[example.png]\n[ta.png]\n[ta1.png]\n[ta2.png]\n[ta3.png]\n[ta4.png]\n[ta5.png]\n[ta6.png]\n[ta7.png]\n[spacer.png]\n<p style='font-weight:bold;text-transform:uppercase;letter-spacing:.5px;'>{Website}</style>\n[ta8.png]\n"
+	},
+    
+    	{
+		"folder":"curious",
+		"name":"The Curious Incident",
+		"thumb":"thumb.png",
+		"content":"I typeset Mark Haddon&rsquo;s Curious Incident of the Dog in the Night-time, a story told from the point of view of an autistic savant named Christopher. This project was a great opportunity to explore Christopher&rsquo;s systematic world through systems of typographic formatting and color. \nHere, I strove to create a dialogue between the published material and the author by exposing Christopher&rsquo;s strange, quirky voice. Using colored overprinting, I literally introduced a second layer of reading.\n[curious-1.jpg]\n[curious-2.jpg]\n[curious-3.jpg]\n[curious-4.jpg]\n[curious-5.jpg]\n[curious-6.jpg]\n[curious-7.jpg]\n[curious-8.jpg]\n[curious-9.jpg]\n[curious-10.jpg]\n[curious-11.jpg]"
+	},
+    
+    
 
 	{
 		"folder":"chocolee",
@@ -21,20 +56,26 @@ work=[
 		"content":"Inspired by origami and Japanese wrapping cloths called furoshiki, &ldquo;Chocolee,&rdquo; chocolates feature patterns designed with both collaged and digitally drawn food elements. Each square wrapper can be easily repurposed for paper-folding and crafting.\nThis imaginary chocolate brand emphasizes not only beautiful wrapping but the fun in its re-use, with paper you might give a second life as gift wrap or even a paper crane. By paying attention to the art of wrapping, we bring moments of aesthetic pleasure and play into everyday life.\nFeatured on Packaging Design Served, Behance Student Show, and The Design Blog, Dec 2013.\n[chocolee.png]\n[chocolee-flavors.gif]\n[chocolee-set-2.jpg]\n[chocolee-business.jpg]\n[chocolee-business-set.jpg]\n[chocolee-pepper-3.jpg]"
 	},
 	
-//	{
-//    	"folder":"mia",
-//		"name":"Miami International Airport",
-//		"thumb":"mia8.png",
-//		"content":"Redesigned directory for the Miami Internation Airport. It was really important that the interface was legible, streamlined, and friendly. My target user was a tired traveler, perhaps experiencing a layover with time to burn. I wanted searching to be efficient and exploration to be fun.\nCheck out the <u><a href='https://projects.invisionapp.com/share/Q2UMXDGF' target='_blank'>InVision prototype.</u></a>\n[mia8.png]\n[mia2.png]\n[mia3.png]\n[mia4.png]\n[mia5.png]\n[mia6.png]\n[mia7.png]"
-//	},
+    
+        
 
-	{
+    
+    {
 		"folder":"places",
 		"name":"Oh, The Places You&rsquo;ll See!",
 		"thumb":"places.png",
 		"content":"A study of simultaneous contrast, the phenomenon where surrounding colors cause two different colors to look relatively more or less similar.\nMy family loves to travel, and this postcard set compiles many of the places I&rsquo;ve seen while looking at color relationships.\nEach postcard has a fun little blurb for what to do during day or night.\n[places-1.jpg]\n[places-2.gif]\n[places-3.jpg]\n[places-4.jpg]\n[places-5.jpg]\n[places-6.gif]\n[places-7.jpg]\n[places-8.jpg]\n[places-9.jpg]\n[places-10.gif]"
 	},
-	// {
+	
+        {
+		"folder":"wildest-flowers",
+		"name":"Wildest Flowers",
+		"thumb":"wildest-flowers.png",
+		"content":"An exploration of duality and the color wheel for Aki Nurosi&rsquo;s Color Theory class. We were prompted to look at how the same color reacts differently on a black vs. a white background. I created these seed packets for flowering weeds like dandelions and clovers. RISD 2013.\n[wildest-flowers-1.jpg]\n[wildest-flowers-2.jpg]\n[wildest-flowers-3.jpg]\n[wildest-flowers-4.jpg]\n[wildest-flowers-5.jpg]\n[wildest-flowers-6.jpg]\n[wildest-flowers-7.jpg]\n[wildest-flowers-8.jpg]\n[wildest-flowers-9.jpg]\n[wildest-flowers-10.jpg]"
+	},
+
+    
+    // {
 	// 	"folder":"berlewi",
 	// 	"name":"Berlewi",
 	// 	"thumb":"berlewi.png",
@@ -46,12 +87,8 @@ work=[
 //		"thumb":"frutiger.png",
 //		"content":"{Set of panels on the life and work of Swiss type designer Adrian Frutiger. 2013.}\n[Panel-1.jpg]\n[Panel-2.jpg]\n[Panel-3.jpg]"
 //	},
-	{
-		"folder":"curious",
-		"name":"The Curious Incident",
-		"thumb":"curious.png",
-		"content":"I typeset Mark Haddon&rsquo;s Curious Incident of the Dog in the Night-time, a story told from the point of view of an autistic savant named Christopher. This project was a great opportunity to explore Christopher&rsquo;s systematic world through systems of typographic formatting and color. \nHere, I strove to create a dialogue between the published material and the author by exposing Christopher&rsquo;s strange, quirky voice. Using colored overprinting, I literally introduced a second layer of reading.\n[curious-1.jpg]\n[curious-2.jpg]\n[curious-3.jpg]\n[curious-4.jpg]\n[curious-5.jpg]\n[curious-6.jpg]\n[curious-7.jpg]\n[curious-8.jpg]\n[curious-9.jpg]\n[curious-10.jpg]\n[curious-11.jpg]"
-	},
+    
+
     
 	// {
 	// 	"folder":"lp",
@@ -75,21 +112,15 @@ work=[
 //	},
 
 
+//
+//	{
+//    	"folder":"pnp",
+//		"name":"Paper Nor Plastic",
+//		"thumb":"thumb.png",
+//		"content":"{A poster triptych on the everyday impact our choices can make.}\n[plastic.jpg]\n[paper.jpg]\n[byo.jpg]\n"
+//	},
+    
 
-	{
-    	"folder":"pnp",
-		"name":"Paper Nor Plastic",
-		"thumb":"thumb.png",
-		"content":"{A poster triptych on the everyday impact our choices can make.}\n[plastic.jpg]\n[paper.jpg]\n[byo.jpg]\n"
-	},
-    
-    {
-    	"folder":"touchart",
-		"name":"TouchArt",
-		"thumb":"ta.png",
-		"content":"How has digital proliferation changed they way we access famous images? Works of art that once existed as location-specific, single editions are now available to save and manipulate on personal computers.\nIn this virtual gallery space, Touch Art,I invited peers and fellow art-lovers to bring themselves closer to their favorite paintings by digitally “touching,” works of art. Through a specific set of instructions, I managed to collect a slew of famous masterworks that had been intruded on by another’s hand. The original set of instructions was publicized on Facebook, but other versions were later distributed through email, Tumblr, and word of mouth.\nContribute your favorite painting at <a href='http://www.touch-art.tumblr.com' target='blank' style='font-weight:bold'>touch-art.tumblr.com</a>\n[ta.png]\n[ta1.png]\n[ta2.png]\n[ta3.png]\n[ta4.png]\n[ta5.png]\n[ta6.png]\n[ta7.png]\n[ta8.png]\n[ta-computer.png]\n"
-	},
-    
     
 //	{
 //		"folder":"kathy-friends",
@@ -116,13 +147,3 @@ quotes=[
 
 
 
-/*Kathy's hew hew area*/
-/*\n[chocolee-milk.jpg]\n[chocolee-white.jpg]\n[chocolee-dark.jpg]\n[chocolee-orange.jpg]\n[chocolee-raspberry.jpg]\n[chocolee-hazelnut.jpg]\n[chocolee-mint.jpg]\n[chocolee-pepper-2.jpg]*/
-
-splashes=[
-	"splash-1.jpg",
-	"splash-5.jpg",
-	"splash-2.jpg",
-	"splash-3.jpg",
-	"splash-4.jpg"
-]
