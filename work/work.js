8220f66b71ab2d10d09545e737f478489b9a101b
Kathy Wu <kathywu@Kathys-MBP.home>
2017-02-13 22:16:10 -0500

    cleaned up the alignment in WORK.

--- work/work.js
+++ work/work.js
@@ -17,7 +17,7 @@ work=[
 
 
 		<br><br>
-		<b>{1. Volume, Weight}</b>
+		<b>1. VOLUME, WEIGHT</b>
 
 		According to Kenneth Goldsmith, the American networked citizen consumed 100,000 words per day in 2008.   If our informational input could be measured in pounds, imagine how it would feel to literally consume or hold that much weight. 
 		
@@ -26,7 +26,7 @@ work=[
 		[dp_1.png]
 		<br>
 		<br>
-		<b>{2. Conversion Tools}</b>
+		<b>2. CONVERSION TOOLS</b>
 		The human body understands the material world and has unique intuitions that help us navigate it. I was interested in how people would use intuition to convert their understanding of virtual units into physical objects. 
 		 I created interpretive “conversion tools”  in the form of frames and boxes.  Each of these symbolized a space for an assigned virtual unit where people’s responses/objects would live.
 		[crowdsourced.png]
@@ -34,7 +34,7 @@ work=[
 		
 		<br>
 		<br>
-		<b>{3. Writing}</b>
+		<b>3. WRITING</b>
 		Writing was an important part of my process and discovery. I generated about 1MB worth of text and images throughout this project.       I populated that data into a <a href='https://drive.google.com/file/d/0BzXTWcuY86iYcHFTREVrbVhrRzA/view?usp=sharing
 ' target='_blank'>process document</a> and a video (below).
 		In the Byte Catalogue, I documented objects I crowd-sourced. I compared these against virtual experiences—for example, the Lord of the Rings trilogy in bytes. 
@@ -44,7 +44,7 @@ work=[
 
 		<br>
 		<br>
-		<b>{4. Ownership of Virtual Things}</b>
+		<b>4. OWNERSHIP OF VIRTUAL THINGS</b>
 		Inspired by the Million Dollar website,  I was amused at the skeuomorph of buying “space” on the Internet.
 		[7_alt.png]
 		<br>
@@ -54,7 +54,7 @@ work=[
 
 		<br>
 		<br>
-		<b>{5. Virtual World}</b>
+		<b>5. VIRTUAL WORLD</b>
 		Eventually I collected enough to fill up a room with artifacts. I found textual objects and materials of different weights and densities and quantified them in bytes.
 		The piece also featured sculptural metaphors like downloading across glass jars, as well as plastic physical hard-drives.
 		[1.png]
@@ -65,7 +65,7 @@ work=[
 		<br>
 
 		
-		<br><br>{<a href='#work'>Back to All Projects</a>}`
+		<br><br>{<a href='#work'>SEE MORE PROJECTS</a>&emsp;&#128064;}`
 	},
 
 	    {
@@ -83,34 +83,34 @@ work=[
 		
 		
 		Still, we already rely so much on text interfaces in our relationships. We challenged ourselves to think more about the body: <b> What are other, sensory ways we can communicate presence?</b> 
-		[spacer.png]
+		<br><br>
 
-		{<b>Prototype 1: Vital Signs</b>}
+		<b>PROTOTYPE 1: VITAL SIGNS</b>
 	
 		We wanted to make people feel to strangers they couldn't see or touch. We imagined a phone calls that used callers' pulse.
 
 		[process1.png]
 		[process2.png]
-		[spacer.png]
+		<br>
 		We tested this idea with <b>LEDs, silicone hearts, and a microphone set.</b> People got a kick out of it, although it lacked a sense of purpose. What was the caller's motivation for seeking out another person?
 		[heart-gif.gif]
-		[spacer.png]
+		<br><br>
 		
-		{<b>Prototype 2: Silent Theater</b>}
+		<b>PROTOTYPE 2: SILENT THEATER</b>
 
 		In later explorations, we opted for subtler interactions. We wanted visitors to feel a bit of objective, so we made a simple story for them to co-complete.
 		Here, we prototyped whimsical characters in a servo-powered puppet theater.
 		[7.png]
-		[spacer.png]
+		<br><br>
 
-		{<b>Prototype 3: Remote Duet</b>}
+		<b>PROTOTYPE 3: REMOTE DUET</b>
 		We still wanted to explore simple narratives, though pulse-driven hardware proved to be technically tricky.
 		Here, we created a <b>stripped-down experience of touch and sound.</b> Gallery visitors in Providence, RI sat across from an invisible partner in Cambridge, MA, and could complete part of a duet by placing their hand on the table. 
 		[8.jpg]
 		Providence supplied the melody, and Cambridge supplied the percussion. While we started off interested in sustained, private experiences, our last idea was mostly public and ambient. On the night of the show, the whole room perked up when someone was on the other side. 
 		[12.jpg]
 		For me, this project was an awesome learning experience for quick prototyping. Limited tools and time meant our work had to evolve quickly. I learned to be less precious with ideas, and to pick up new skills on the fly.
-		<br><br>{<a href='#work'>Back to All Projects</a>}`
+		<br><br>{<a href='#work'>SEE MORE PROJECTS</a>&emsp;&#128064;}`
 
 	},
 
@@ -120,18 +120,18 @@ work=[
 		"tags":"Web, Interactive, Poetics",
 		"thumb":"thumb.png",
 		"content":`Printed text marries an immutable form, but a pixelated grapheme on the screen can follow a dynamic and multi-modal existence. How can different sensory channels in the fluid web reading-space create new dimensions of readership? Text already transports readers online; maybe it can also become emotive, responsive, even synesthetic. Digital poetic experiments and sketches.
-		[spacer-half.png] 
+		<br>
 		<b>MONTH OF MAY:</b>&ensp;Recombinative love poems. <a href='digpo/may.html' target='_blank'>Make your own.</a>
 		[may.png]
 		[may-3.gif]
-		[spacer.png]
+		<br><br>
 		<b>BASHO IN THE FOREST:</b>&ensp; Haikus to stumble upon in the woods. <a href='digpo/garden/basho.html' target='_blank'>Take a gander?</a>
 		[basho.png]
 		[basho1.png]
-		[spacer.png]
+		<br><br>
 		<b>GRECIAN URNS:</b>&ensp; Online-only artifacts made from letters. Please <a href='ffabschrift/pottery.html' target='_blank'>do not touch.</a>
 		[pottery.gif]
-		<br><br>{<a href='#work'>Back to All Projects</a>}`
+		<br><br>{<a href='#work'>SEE MORE PROJECTS</a>&emsp;&#128064;}`
 	},
  
 
@@ -155,12 +155,13 @@ work=[
 		During the hackathon demo, we invited everyone in the audience to join the game. At one point, we had over 200 participants going crazy on-screen. 
 		[demo2.JPG]
 		Testing with judges and students. Our gamers said it was pretty addictive!
-		[spacer.png]
-		<b>{The Game}</b>
-		{It's not online yet. In the meantime, here's some graphics.}[motemote-3.gif]
+		<br><br>
+		<b>THE GAME</b>
+		It's not online yet. In the meantime, here's some graphics.
+		[motemote-3.gif]
 		[motemote.gif]
 		[motemote-2.gif]
-		<br><br>{<a href='#work'>Back to All Projects</a>}`
+		<br><br>{<a href='#work'>SEE MORE PROJECTS</a>&emsp;&#128064;}`
 	},
 
    {
@@ -172,26 +173,26 @@ work=[
 		Bumble is a <b>collaborative fiction-building tool.</b> Users can create stories with their thumbs, and navigate a web of stories like a dungeon map. I collaborated with Kevin Cadena and Jamar Bromley in the studio course, Computer Utopias.
 		[thumb2.png]
 		<br><br>
-		<b>{What is the shape of mobile media?}</b>
+		<b>What is the shape of mobile media?</b>
 		Today, even with responsive design, much of mobile media retrofits desktop sites to a small screen. Scrollable content is not comfortable or “native” to this device.
 		Our team was inspired by Hypercard and SmallTalk, which informed the shape of media-making on personal computers in the 1960s and beyond. 
 		[2.png]
 		Our early sketches explored hyperlinkable, inspectable cards, where people could create webcontent without touching a full-size computer.
 		The most difficult question was nailing down <b>what media our tool would enable.</b> Ephemeral, or invested? Individual, or collaborative?
 		<br><br>
-		<b>{VERSION 1}</b>
+		<b>VERSION 1</b>
 		Mobile devices encourage creative manipulation of images and videos. We were drawn to playful media, and inspired by Snapchat and Kidpix. In this iteration of Bumble, users could build <b>expressive webcards on a social network.</b>
 		[4.png]
 		The program was feature-loaded: color, text, audio, shape, stickers, and hyperlinks! But what are they making, and would they care?</b>
 		<b>What we learned:</b> The timeframe for mobile content creation, in this case, was less invested than on desktop. We needed to serve this platform's attention span.
 		<br><br>
-		<b>{VERSION 2}</b>
+		<b>VERSION 2</b>
 		We challenged our initial assumption that people would want to craft handheld websites with care. We trashed all content primitives except video and scribbling.
 		[6.jpg]
 		Oops… so we made Snapchat’s less cool cousin. We lost our initial mission, which was to redefine what it meant to make and move in a mobile web browser.
 		<b>What we learned:</b> It's better to make something that 100 people love than something for the masses to ignore. Make a monopoly, not more competition.
 		<br><br>
-		<b>{VERSION 3 — Storytelling redefines our vision.}</b>
+		<b>VERSION 3 — Storytelling redefines our vision.</b>
 		Up until this point, we struggled to articulate who would use this, and why the phone matters. Full-blown websites were too heavy-duty for mobile, and Snapchat had already been done.
 	
 		In our final iteration, we created a storytelling platform fueled by <b>LARP culture</b>.
@@ -204,7 +205,7 @@ work=[
 		
 		In our first version, Bumble serves a niche audience of <b>role-playing enthusiasts.</b> In the future, Bumble can grow to accommodate new content. Instructional threads could draw in new networks of crafty people demonstrating skills and recipes. Or, similar to Reddit, people from all over the world can respond to a single thread prompt, with infinite branches.
 		<br><br>
-		{<a href='#work'>Back to All Projects</a>}`
+		{<a href='#work'>SEE MORE PROJECTS</a>&emsp;&#128064;}`
 	},
     
     
@@ -226,7 +227,7 @@ work=[
 		[1.png]
 		[3.png]
 		[5.png]
-		<br><br>{<a href='#work'>Back to All Projects</a>}`
+		<br><br>{<a href='#work'>SEE MORE PROJECTS</a>&emsp;&#128064;}`
 		
 	},   
 
@@ -235,7 +236,22 @@ work=[
 		"name":"Touch Art",
 		"tags":"Web, Fun",
 		"thumb":"thumb.png",
-		"content":"Touch Art is a crowdsourced collection of virtual paintings touched by real humans.\nDigital proliferation means that works of art that once existed as location-specific, single editions are now available to save and manipulate on personal computers. You can't really smear your fingers all over a Singer Sargent in person. But how about on Google Images? \n[example_zoom.png]\n<br>I created instructions for fellow art-lovers to digitally intrude on favorite paintings, using their own pixelated hand. I asked them to find a painting on Google Images and just intrude on it with their virtual hand. Some of them got pretty creative...\n[art1.png]\n[art3.png]\n[art4.png]\n[art.png]\n[spacer.png]\n<b>{THE COLLECTION}</b>\nYou can see all the artwork in a <a href='http://touch-art.tumblr.com/' target='blank' style='font-weight:bold'>virtual gallery space.</a> Everyone who partipated was also published in a gallery catalog:\n[ta.png]\n[ta4.png]\n[ta5.png]\n<br><br>{<a href='#work'>Back to All Projects</a>}"
+		"content":`
+		Touch Art is a crowdsourced collection of virtual paintings touched by real humans.
+		Digital proliferation means that works of art that once existed as location-specific, single editions are now available to save and manipulate on personal computers. You can't really smear your fingers all over a Singer Sargent in person. But how about on Google Images? 
+		[example_zoom.png]
+		<br>I created instructions for fellow art-lovers to digitally intrude on favorite paintings, using their own pixelated hand. I asked them to find a painting on Google Images and just intrude on it with their virtual hand. Some of them got pretty creative...
+		[art1.png]
+		[art3.png]
+		[art4.png]
+		[art.png]
+		<br><br>
+		<b>THE COLLECTION</b>
+		You can see all the artwork in a <a href='http://touch-art.tumblr.com/' target='blank' style='font-weight:bold'>virtual gallery space.</a> Everyone who partipated was also published in a gallery catalog:
+		[ta.png]
+		[ta4.png]
+		[ta5.png]
+		<br><br>{<a href='#work'>SEE MORE PROJECTS</a>&emsp;&#128064;}`
 	},
 
 
@@ -249,25 +265,24 @@ work=[
 		I first wanted to know more about Turrell's work. What stood out to me was how he distills nature into this pure, temporal experience. His work focuses and frames natural processes—like the sunset— so the viewer enters this almost meditative state, where they are hyper-aware of passing time.
 		Here's some early research: obsessive moodboarding, drawing, and word studies.
 		[process4.png]
-		
 		[process2.png]
 		I explored pages of logomarks, starting on paper, and also playing with vectors, shadows, and cut vellum paper. 
 		[process1.png]
-		[spacer.png]
-		<p style='font-weight:bold;text-transform:uppercase;letter-spacing:.5px;'>{LOGO & APPLICATION}</style>
+		<br><br>
+		<b>LOGO & APPLICATION</b>
 		The final mark references a slow, recession of light, space, and atmosphere, while winking at the “T” of Turrell. The palette is a saturated take on the gradation of daylight, and the colors of the Turrell’s native Arizona landscape. [turrell-01.svg]
 		[turrell-02.svg]
 		[stationary.jpg]
 		[turrell12.png]
 		[turrellbooks.png]
 		[poster.jpg]
-		[spacer.png]
-		<p style='font-weight:bold;text-transform:uppercase;letter-spacing:.5px;'>{THE MUSEUM EXPERIENCE}</style>
+		<br><br>
+		<b>THE MUSEUM EXPERIENCE</b>
 		The way a Turrell piece transforms when viewed during different times of the day inspired these dynamic tickets. The museum, open from 10:00AM to 6:00PM daily, issues a different colored ticket to visitors each hour. Viewed as a long strip, the tickets appear as a continuous gradient. 
 		[tickets-02.png]
 		What stood out to me about the museum's location was how beautiful the surrounding wineries were. This outdoor installation would bring visitors together in a tranquil museum garden. The pool connects to an unseen irrigation system which waters the nearby vines. In this way, the piece feeds back into the terrain, the way a Turrell piece works together with nature.
 		[reflect_gif.gif]
-		<br><br>{<a href='#work'>Back to All Projects</a>}`
+		<br><br>{<a href='#work'>SEE MORE PROJECTS</a>&emsp;&#128064;}`
 	},
 
 
@@ -284,7 +299,7 @@ work=[
 		[chocolee.png]
 		[chocolee-set-2.jpg]
 		[chocolee-flavors.gif]
-		<br><br>{<a href='#work'>Back to All Projects</a>}`
+		<br><br>{<a href='#work'>SEE MORE PROJECTS</a>&emsp;&#128064;}`
 	},
 
 
@@ -294,14 +309,14 @@ work=[
 // 		"tags":"Print, Packaging",
 // 		"thumb":"places-7.jpg",
 // 		"content":`Select work from Aki Nurosi's Color Studio at RISD, 2013. These were 2-3 week studies applying observations from Josef Alber's Interaction of Color. Curious to learn more? Check out the <a href='https:\/\/drive.google.com/file/d/0BzXTWcuY86iYa0xxZDJlREdsZkE/view?usp=sharing' target='_blank'>process book.</a>
-// 		[spacer.png]
+// 		<br><br>
 // 		<p style='font-weight:bold;text-transform:uppercase;letter-spacing:.5px;'>{The WILDEST FLOWERS}</style>
 // 		A color wheel exploration for Aki Nurosi&rsquo;s Color Theory class. We were prompted to look at how the same color reacts differently on a black vs. a white background. I created these seed packets for flowering weeds like dandelions and clovers. 
 // 		[wildest-flowers-1.jpg]
 // 		[wildest-flowers-3.gif]
 // 		[wildest-flowers-7.gif]
 // 		[wildest-flowers-9.jpg]
-// 		[spacer.png]
+// 		<br><br>
 // 		<p style='font-weight:bold;text-transform:uppercase;letter-spacing:.5px;'>{Oh, The Places You'll See!}</style>
 // 		A study of simultaneous contrast, the phenomenon where surrounding colors cause two different colors to look relatively more or less similar.
 // 		My family loves to travel, and this postcard set compiles many of the places I&rsquo;ve seen while looking at color relationships.
@@ -310,7 +325,7 @@ work=[
 // 		[places-5.jpg]
 // 		[places-6.gif]
 // 		[places-7.jpg]
-// 		<br><br>{<a href='#work'>Back to All Projects</a>}`
+// 		<br><br>{<a href='#work'>SEE MORE PROJECTS</a>&emsp;&#128064;}`
 // 	},
 
 
@@ -327,7 +342,7 @@ work=[
 	// 	[curious-5.jpg]
 	// 	[curious-10.jpg]
 	// 	[curious-11.jpg]
-	// 	<br><br>{<a href='#work'>Back to All Projects</a>}`
+	// 	<br><br>{<a href='#work'>SEE MORE PROJECTS</a>&emsp;&#128064;}`
 	// },
 
  
@@ -351,7 +366,7 @@ work=[
 		<br>
 		{<video width=100% src='work/shapinglanguage/speech-to-text.mp4' controls autoplay='on' muted></video>}
 		<br><br>
-		{<a href='#work'>Back to All Projects</a>}
+		{<a href='#work'>SEE MORE PROJECTS</a>&emsp;&#128064;}
 		`
 },
     
@@ -373,7 +388,7 @@ work=[
 		[sketch4.jpg]
 		[sketch5.jpg]
 		[sketch6.jpg]
-		<br><br>{<a href='#work'>Back to All Projects</a>}`
+		<br><br>{<a href='#work'>SEE MORE PROJECTS</a>&emsp;&#128064;}`
 	},
 
 
