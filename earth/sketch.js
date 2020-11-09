32e488d8f2b7ee4d1c184a102aaf3405ada80929
Kathy Wu <kwu01@risd.edu>
2020-11-09 00:49:32 -0500

    added earth project

---
+++ earth/sketch.js
@@ -0,0 +1,71 @@
+
+// //  let data;
+//  let next = 0;
+//  let string;
+
+// // let img1;
+// // let img2;
+// // let img3;
+// // let img4;
+// // let img5;
+// // let img6;
+// // let imgDisplay;
+// // let imgArray = [];
+
+// // let thankyou;
+// // let mainpart;
+// // let everything;
+
+// let variable = "poop";
+// let param2 = "dum";
+
+
+//  function preload() {
+//   let url = 'objects.json';
+//   data = loadJSON(url);
+
+// }
+//  function setup() {
+//  	createCanvas(800, 600);
+//  	background(0);
+
+//  	textSize(40);
+//  	textAlign(CENTER);
+
+//  	// mainpart.play();
+
+//  }
+
+//  function draw() {
+// 	fill(255);
+//  	// image(img4, 0, 0, width, height);
+//   string = `Earth is now called ${variable} and is overrun with ${param2}`
+// 	text(string, width/2, height/2);
+
+//   print(data(2,1))
+
+//  }  
+
+// function keyPressed(){
+// 	incrementText();
+
+
+// }
+
+//  function incrementText(){
+//  	if (next < 1000 - 1) {
+//  			next++; 
+ 
+
+ 
+//  		}
+//  		else {
+//  			next = 0;
+//  		}
+//  }
+
+// function incrementImg(){
+//  	imgDisplay = random(imgArray);
+//  }
+
+
